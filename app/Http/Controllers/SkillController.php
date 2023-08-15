c4bbfb97621a80e23989e501b6fc97bff13fd0dd
Josua Perez <josuap63@gmail.com>
2023-08-15 15:03:24 -0400

    ✨ added feature skill

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\SkillResource;
+use App\Models\Skill;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class SkillController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        $skills = SkillResource::collection(Skill::all());
+
+        return Inertia::render('Skills/Index', compact('skills'));
     }
 
     /**
@@ -19,46 +28,73 @@ class SkillController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Skills/Create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
-    }
+        $request->validate([
+            'image' => ['required', 'image'],
+            'name' => ['required', 'min:3'],
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('skills');
+
+            Skill::create([
+                'name' => $request->name,
+                'image' => $image,
+            ]);
+
+            return Redirect::route('skills.index');
+        }
+
+        return Redirect::back();
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Skill $skill)
     {
-        //
+        return Inertia::render('Skills/Edit', compact('skill'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Skill $skill): RedirectResponse
     {
-        //
+        $image = $skill->image;
+
+        $request->validate([
+            'name' => ['required', 'min:3']
+        ]);
+
+        if ($request->hasFile('image')) {
+            Storage::delete($skill->image);
+            $image = $request->file('image')->store('skills');
+        }
+
+        $skill->update([
+            'name' => $request->name,
+            'image' => $image
+        ]);
+
+        return Redirect::route('skills.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Skill $skill)
     {
-        //
+        Storage::delete($skill->image);
+        $skill->delete();
+
+        return Redirect::back();
     }
 }
