a4a54bb0ac796f003b1e672d47e58c3972a7e14f
Josua Perez <josuap63@gmail.com>
2023-08-15 15:04:26 -0400

    🎨 added feature Projects

--- database/migrations/2023_08_14_181751_create_projects_table.php
+++ database/migrations/2023_08_14_181751_create_projects_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('image');
             $table->string('project_url')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
