fb4ecb901053cf071aef292dbdf2463e3321b99c
Josua Perez <josuap63@gmail.com>
2023-08-15 15:04:07 -0400

    🎨 added feature Projects

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+use App\Models\Skill;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        $projects = ProjectResource::collection(Project::with('skill')->get());
+
+        return Inertia::render('Projects/Index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): Response
+    {
+        $skills = Skill::all();
+
+        return Inertia::render('Projects/Create', compact('skills'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'image' => ['required', 'image'],
+            'name' => ['required', 'min:5'],
+            'skill_id' => ['required'],
+        ]);
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('projects');
+
+            Project::create([
+                'skill_id' => $request->skill_id,
+                'name' => $request->name,
+                'image' => $image,
+                'project_url' => $request->project_url,
+            ]);
+
+            return Redirect::route('projects.index');
+        }
+
+        return Redirect::back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Projects/Index.vue
@@ -0,0 +1,87 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head, Link} from '@inertiajs/vue3';
+
+defineProps({
+    projects: Object
+})
+</script>
+
+<template>
+  <Head title="Projects"/>
+
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Projects</h2>
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex justify-end m-2 p-2">
+          <Link
+              :href="route('projects.create')"
+              class="
+                px-4
+                py-2
+                bg-indigo-500
+                hover:bg-indigo-700
+                text-white
+                rounded-md"
+          >
+            New project
+          </Link>
+        </div>
+
+          <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                ID
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Skill
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Image
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr
+                            v-for="project in projects.data"
+                            :key="project.id"
+                            class="bg-white border-b"
+                        >
+                            <th scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                {{ project.id }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ project.name }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ project.skill.name }}
+                            </td>
+                            <td class="px-6 py-4">
+                                <img class="w-11 h-11" :src="project.image"/>
+                            </td>
+                            <td class="px-6 py-4">
+                                delete/edit
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+      </div>
+    </div>
+  </AuthenticatedLayout>
+</template>
+
