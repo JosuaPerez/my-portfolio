ff9644950d9d9a617276a580ddf938536eccb141
Josua Perez <josuap63@gmail.com>
2023-08-16 13:22:26 -0400

    🎨 config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -11,11 +11,39 @@ export default {
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        fontFamily: {
+            primary: "Playfair Display",
+            body: "work Sans",
+        },
+        container: {
+            padding: {
+                DEFAULT: "1rem",
+                lg: "3rem",
             },
         },
+        extend: {
+            colors: {
+                "light-primary": "#F6F4EB",
+                "light-secondary": "#91C8E4",
+                "light-tail-100": "#749BC2",
+                "light-tail-500": "#4682A9",
+
+                "dark-primary": "#272829",
+                "dark-secondary": "#61677A",
+                "dark-navy-100": "#D8D9DA",
+                "dark-navy-500": "#FFF6E0",
+
+                accentColor: {
+                    DEFAULT: "#ac6b34",
+                    hover: "#925a2b"
+                },
+                paragraph: "#878e99",
+                blue: colors.blue,
+                indigo: colors.indigo,
+                green: colors.green,
+                red: colors.red
+            }
+        }
     },
 
     plugins: [forms],
