40bdd9d1af03a776379bb53be952ad638129b15c
Josua Perez <josuap63@gmail.com>
2023-08-17 17:12:04 -0400

    🎨 Added Hero Section

---
+++ resources/js/Components/Frontend/Hero.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+    <section id="home"
+             class="lg:h-[85vh] flex items-center bg-light-primary dark:bg-dark-primary py-32 lg:py-0 overflow-hidden">
+        <div class="container mx-auto h-full">
+            <div class="flex flex-col md:flex-row items-center h-full">
+                <div class="flex-1 flex flex-col items-center lg:items-start">
+                    <p class="text-lg text-accentColor text-md mb-[22px]">
+                        Hey, I'm Josua! 👋
+                    </p>
+                    <h1 class="text-4xl leading-[44px] md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px]">
+                        I Build & Design <br/> Web Interface
+                    </h1>
+                    <p class="pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center lg:text-left">
+                        I build and design web interfaces that combine functionality with captivating aesthetics. My
+                        goal is to take
+                        ideas from conception to digital reality, creating solutions that are not only beautiful to look
+                        at, but
+                        also intuitive and efficient.
+                    </p>
+                    <button
+                        class="rounded-xl btn btn-md text-white bg-accentColor text-white md:btn-lg transition-all">
+                        Work with me
+                    </button>
+                </div>
+                <div class="flex flex-1 justify-end items-center h-full mt-8 md:mt-0">
+                    <img class="rounded-lg" src="http://portfolio-app.test/img/hero.jpg" alt="Hero"/>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
