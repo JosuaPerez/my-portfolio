788bc93e0973a407bce5a0ab8860e109e3dd47b8
Josua Perez <josuap63@gmail.com>
2023-08-21 19:21:00 -0400

    🚧 wip

---
+++ resources/js/Components/Frontend/FloatingButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="fixed bottom-4 right-4">
+        <button @click="changeLanguage('es')"
+                class="bg-blue-500 hover:bg-blue-600 text-white rounded-full p-3 shadow-md w-12 h-12">
+            <!-- Puedes personalizar el contenido del botón aquí -->
+            <i class="fas fa-plus"></i>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    changeLanguage(locale) {
+      this.$i18n.locale = locale;
+    }
+  }
+};
+</script>
